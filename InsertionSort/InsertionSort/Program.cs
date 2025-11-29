bfc35cb07409cd0dc8a31114ebb2abbe84d7e37e
faroukbelaid09 <faroukbelaid9@gmail.com>
2025-11-29 17:02:56 +0300

    implementing an insertion sort algorithm

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    internal class Program
+    {
+        static void InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = key;
+            }
+        }
+        static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] arr = { 6,4,2,5,3,1};
+
+            Console.WriteLine("Original Array: ");
+            PrintArray(arr);
+
+            InsertionSort(arr);
+
+            Console.WriteLine("\n\nSorted Array Using Insertion Sort:");
+            PrintArray(arr);
+        }
+    }
+}
